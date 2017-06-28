63d9950224f9151c72cf31650aab8805b583e634
Overxel <antoniorodriguezvega@gmail.com>
2017-06-28 20:37:30 +0200

    actualizado el formulario de contacto

--- practicas/formulario_contacto/index.php
+++ practicas/formulario_contacto/index.php
@@ -1,5 +1,40 @@
 <?php 
 
+$errores = '';
+$enviado='';
+if (isset($_POST['submit'])) { //esto quiere decir que si el usuario presiono el boton de enviar, que haga lo siguiente:...//
+
+	# code...
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+if (!empty($nombre)) {
+
+	# code...
+	$nombre = trim($nombre);
+	$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+
+} else {
+
+	$errores .= 'Por favor ingresa un nombre <br/>';
+}
+
+if (!empty($correo)) {
+
+	$correo = trim($correo);
+	$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+	if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) { //devuelve un false si el email no es válido, si el email es válido manda el mail por, ejemplo fulanito@gmail.com 
+		$errores .= 'Por favor ingresa un correo válido <br/>';
+	} 
+	# code...
+} else {
+
+		$errores .= 'Por favor ingresa un correo <br/>';
+	}
+
+}
+
 require 'index.view.php';
 
 
--- practicas/formulario_contacto/index.view.php
+++ practicas/formulario_contacto/index.view.php
@@ -14,10 +14,23 @@
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 	
 <input type="text" class="form-control" name="nombre" placeholder="Nombre: " value="" id="nombre">
+
+
 <input type="email" class="form-control" name="correo" placeholder="Correo: " value="" id="correo">
+
+
 <textarea name="mensaje" class="form-control" placeholder="Mensaje:" id="mensaje"></textarea>
 
+<?php if (!empty($errores)): ?>
+	<div class="alert error">
+		<?php echo $errores; ?>
+	</div>
+<?php elseif($enviado): ?>
+	<div class="alert success">
+		<p>Enviado correctamente</p>
+	</div>
 
+<?php endif ?>
 <input type="submit" name="submit" id="submit" class="btn btn-primary" value="Enviar Correo">
 </form>
 
